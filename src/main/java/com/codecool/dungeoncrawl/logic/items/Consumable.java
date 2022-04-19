a6fb73bd46c5858896318a54bab53deea7a8281e
dudaskobende <dudaskobende@gmail.com>
2022-04-19 12:46:39 +0200

    Add Consumable item and Consumable types

--- src/main/java/com/codecool/dungeoncrawl/logic/items/Consumable.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Consumable.java
@@ -1,4 +1,21 @@
 package com.codecool.dungeoncrawl.logic.items;
 
-public class Consumable {
+import com.codecool.dungeoncrawl.logic.Cell;
+
+public class Consumable extends Item {
+    private final ConsumableType consumableType;
+
+    public Consumable(Cell cell, ConsumableType consumableType) {
+        super(cell);
+        this.consumableType = consumableType;
+    }
+
+    @Override
+    public String getTileName() {
+        return consumableType.getTileName();
+    }
+
+    public int getHealthModifier() {
+        return consumableType.getHealthModifier();
+    }
 }
--- src/main/java/com/codecool/dungeoncrawl/logic/items/ConsumableType.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/ConsumableType.java
@@ -1,4 +1,23 @@
 package com.codecool.dungeoncrawl.logic.items;
 
 public enum ConsumableType {
+    APPLE("apple", 5),
+    BREAD("bread", 10),
+    MEAT("meat", 20);
+
+    private final String tileName;
+    private final int healthModifier;
+
+    ConsumableType(String tileName, int healthModifier) {
+        this.tileName = tileName;
+        this.healthModifier = healthModifier;
+    }
+
+    public int getHealthModifier() {
+        return healthModifier;
+    }
+
+    public String getTileName() {
+        return tileName;
+    }
 }
