956fa08b9b89a9e4a2fe6834ef56054d1cc63c65
dudaskobende <dudaskobende@gmail.com>
2022-04-19 09:45:28 +0200

    Add mapLoader rework and level 1

--- src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
@@ -8,40 +8,55 @@ import java.util.Scanner;
 
 public class MapLoader {
     public static GameMap loadMap() {
-        InputStream is = MapLoader.class.getResourceAsStream("/map.txt");
+        String delimiter = ",";
+        InputStream is = MapLoader.class.getResourceAsStream("/custom_map/level_1.csv");
         Scanner scanner = new Scanner(is);
-        int width = scanner.nextInt();
-        int height = scanner.nextInt();
+        scanner.useDelimiter(delimiter);
+        String firstLine = scanner.nextLine();
+        String[] firstLineChars = firstLine.split(delimiter);
+        int width = firstLineChars.length;
+        int height = 1;
+        while (scanner.hasNextLine()) {
+            scanner.nextLine();
+            height++;
+        }
 
-        scanner.nextLine(); // empty line
+        is = MapLoader.class.getResourceAsStream("/custom_map/level_1.csv");
+        scanner = new Scanner(is);
+        scanner.useDelimiter(",");
 
         GameMap map = new GameMap(width, height, CellType.EMPTY);
         for (int y = 0; y < height; y++) {
             String line = scanner.nextLine();
-            for (int x = 0; x < width; x++) {
-                if (x < line.length()) {
-                    Cell cell = map.getCell(x, y);
-                    switch (line.charAt(x)) {
-                        case ' ':
-                            cell.setType(CellType.EMPTY);
-                            break;
-                        case '#':
-                            cell.setType(CellType.WALL);
-                            break;
-                        case '.':
-                            cell.setType(CellType.FLOOR);
-                            break;
-                        case 's':
-                            cell.setType(CellType.FLOOR);
-                            new Skeleton(cell);
-                            break;
-                        case '@':
-                            cell.setType(CellType.FLOOR);
-                            map.setPlayer(new Player(cell));
-                            break;
-                        default:
-                            throw new RuntimeException("Unrecognized character: '" + line.charAt(x) + "'");
-                    }
+            String[] lineChars = line.split(delimiter);
+            for (int x = 0; x < lineChars.length; x++) {
+                Cell cell = map.getCell(x, y);
+                switch (lineChars[x]) {
+                    case "-1":
+                        cell.setType(CellType.EMPTY);
+                        break;
+                    case "0":
+                        cell.setType(CellType.EMPTY);
+                        break;
+                    case "586":
+                        cell.setType(CellType.WALL);
+                        break;
+                    case "32":
+                        cell.setType(CellType.TREE);
+                        break;
+                    case "4":
+                        cell.setType(CellType.FLOOR);
+                        break;
+                    case "93":
+                        cell.setType(CellType.FLOOR);
+                        new Skeleton(cell);
+                        break;
+                    case "27":
+                        cell.setType(CellType.FLOOR);
+                        map.setPlayer(new Player(cell));
+                        break;
+                    default:
+                        throw new RuntimeException("Unrecognized character: '" + line.charAt(x) + "'");
                 }
             }
         }
