3dba00ccafa584204b93ea84b5ac800798ae57f3
dudaskobende <dudaskobende@gmail.com>
2022-04-20 12:25:41 +0200

    Add names for ConsumableType enum and override toString method

--- src/main/java/com/codecool/dungeoncrawl/logic/items/ConsumableType.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/ConsumableType.java
@@ -1,14 +1,16 @@
 package com.codecool.dungeoncrawl.logic.items;
 
 public enum ConsumableType {
-    APPLE("apple", 5),
-    BREAD("bread", 10),
-    MEAT("meat", 20);
+    APPLE("Apple", "apple", 5),
+    BREAD("Bread", "bread", 10),
+    MEAT("Meat", "meat", 20);
 
+    private final String name;
     private final String tileName;
     private final int healthModifier;
 
-    ConsumableType(String tileName, int healthModifier) {
+    ConsumableType(String name, String tileName, int healthModifier) {
+        this.name = name;
         this.tileName = tileName;
         this.healthModifier = healthModifier;
     }
@@ -19,5 +21,10 @@ public enum ConsumableType {
 
     public String getTileName() {
         return tileName;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
