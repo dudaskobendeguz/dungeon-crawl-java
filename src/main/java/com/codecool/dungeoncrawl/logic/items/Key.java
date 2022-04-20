3aa3154807a02cfe718f2333f13ae52afd6525f1
dudaskobende <dudaskobende@gmail.com>
2022-04-20 19:42:57 +0200

    Add doorTypes for key

--- src/main/java/com/codecool/dungeoncrawl/logic/items/Key.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/Key.java
@@ -1,6 +1,7 @@
 package com.codecool.dungeoncrawl.logic.items;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.CellType;
 
 public class Key extends Item {
     private final KeyType keyType;
@@ -17,5 +18,13 @@ public class Key extends Item {
 
     public KeyType getKeyType() {
         return keyType;
+    }
+
+    public CellType getClosedDoorType() {
+        return keyType.getClosedDoorType();
+    }
+
+    public CellType getOpenedDoor() {
+        return keyType.getOpenedDoor();
     }
 }
