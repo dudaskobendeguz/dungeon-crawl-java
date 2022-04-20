bb629a36171d29bf58c5574941851ee50f5d3868
dudaskobende <dudaskobende@gmail.com>
2022-04-20 12:25:52 +0200

    Add names for KeyType enum and override toString method

--- src/main/java/com/codecool/dungeoncrawl/logic/items/KeyType.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/KeyType.java
@@ -1,17 +1,24 @@
 package com.codecool.dungeoncrawl.logic.items;
 
 public enum KeyType {
-    DOOR_KEY("door_key"),
-    CHEST_KEY("chest_key"),
-    LEVEL_KEY("level_key");
+    DOOR_KEY("Door key", "door_key"),
+    CHEST_KEY("Chest key", "chest_key"),
+    LEVEL_KEY("Level key", "level_key");
 
+    private final String name;
     private final String tileName;
 
-    KeyType(String tileName) {
+    KeyType(String name, String tileName) {
+        this.name = name;
         this.tileName = tileName;
     }
 
     public String getTileName() {
         return tileName;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
