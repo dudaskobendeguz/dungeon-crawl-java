03d17cbff33de5513c216dd06bc44ebab775f97a
benedekhalaj <bendihalaj@gmail.com>
2022-04-21 23:03:25 +0200

    Update every Level

--- src/main/java/com/codecool/dungeoncrawl/Main.java
+++ src/main/java/com/codecool/dungeoncrawl/Main.java
@@ -32,7 +32,7 @@ import java.util.TimerTask;
 public class Main extends Application {
     private static String playerName = "Player_1";
     private final static int MAP_SIZE = 15;
-    private Levels currentLevel = Levels.LEVEL_4;
+    private Levels currentLevel = Levels.LEVEL_1;
     GameMap map = MapLoader.loadMap(currentLevel.getMapFilePath(), new Player(playerName));
     Canvas canvas = new Canvas(
             MAP_SIZE * Tiles.TILE_WIDTH,
