7f3450eb178b061552956b03f3dd1edb4f4cf31e
benedekhalaj <bendihalaj@gmail.com>
2022-04-21 11:46:40 +0200

    Add Slime.moveTimer

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Slime.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Slime.java
@@ -3,6 +3,10 @@ package com.codecool.dungeoncrawl.logic.actors;
 import com.codecool.dungeoncrawl.logic.Cell;
 
 public class Slime extends Monster implements Movable {
+
+    private int moveTimer = 0;
+    private static final int MOVE_TIMER_CEILING = 1;
+
     public Slime(Cell cell) {
         super(cell, 20, 1);
     }
@@ -14,6 +18,15 @@ public class Slime extends Monster implements Movable {
 
     @Override
     public void move(int playerX, int playerY) {
-        moveRandomly();
+        setTimer();
+        if (moveTimer == 0) {
+            moveRandomly();
+        }
+    }
+
+    private void setTimer() {
+        if (++moveTimer >= MOVE_TIMER_CEILING) {
+            moveTimer = 0;
+        }
     }
 }
