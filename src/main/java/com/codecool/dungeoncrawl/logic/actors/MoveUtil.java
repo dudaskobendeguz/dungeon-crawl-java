60a8619ecac9d63abec4376fcf5306207ebd4cca
dudaskobende <dudaskobende@gmail.com>
2022-04-21 18:14:48 +0200

    Add MoveUtil.moveRandomly static method

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/MoveUtil.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/MoveUtil.java
@@ -2,8 +2,13 @@ package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
 
+import java.util.Random;
+
 public abstract class MoveUtil {
-    public static Cell moveTowardsPlayer(Cell cell, int playerX, int playerY) {
+    static Random random = new Random();
+
+
+    protected static Cell moveTowardsPlayer(Cell cell, int playerX, int playerY) {
         int x = cell.getX();
         int y = cell.getY();
 
@@ -19,5 +24,22 @@ public abstract class MoveUtil {
             return cell.getNeighbor(0, 1);
         }
         return null;
+    }
+
+    protected static Cell moveRandomly(Cell cell) {
+        int maxRandom = 3;
+        int randomNumber = random.nextInt(maxRandom + 1);
+        switch (randomNumber) {
+            case 0:
+                return cell.getNeighbor(1, 0);
+            case 1:
+                return cell.getNeighbor(-1, 0);
+            case 2:
+                return cell.getNeighbor(0, 1);
+            case 3:
+                return cell.getNeighbor(0, -1);
+            default:
+                return null;
+        }
     }
 }
