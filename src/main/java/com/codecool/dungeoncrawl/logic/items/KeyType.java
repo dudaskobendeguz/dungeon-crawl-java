f68be97533adf5a651984091e63a4042b2d2ebe8
dudaskobende <dudaskobende@gmail.com>
2022-04-24 03:01:32 +0200

    Refactor KeyType enum

--- src/main/java/com/codecool/dungeoncrawl/logic/items/KeyType.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/KeyType.java
@@ -3,25 +3,23 @@ package com.codecool.dungeoncrawl.logic.items;
 import com.codecool.dungeoncrawl.logic.CellType;
 
 public enum KeyType {
-    SIMPLE_DOOR_KEY(
-            754,
-            "Door key",
-            CellType.SIMPLE_DOOR_CLOSED,
-            CellType.SIMPLE_DOOR_OPENED
-    ),
-
     CHEST_KEY(
             752,
             "Chest key",
             CellType.CHEST_CLOSED,
             CellType.CHEST_OPENED
     ),
-
     LEVEL_KEY(
             753,
             "Level key",
             CellType.LEVEL_SWITCH_DOOR_CLOSED,
             CellType.LEVEL_SWITCH_DOOR_OPENED
+    ),
+    SIMPLE_DOOR_KEY(
+            754,
+            "Door key",
+            CellType.SIMPLE_DOOR_CLOSED,
+            CellType.SIMPLE_DOOR_OPENED
     );
 
     private final int ID;
