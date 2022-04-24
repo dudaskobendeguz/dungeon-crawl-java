65abc258f6f6a9868db80b0ae750f727f4eadce0
benedekhalaj <bendihalaj@gmail.com>
2022-04-24 21:43:26 +0200

    Rename Tiles fields and variables

--- src/main/java/com/codecool/dungeoncrawl/Tiles.java
+++ src/main/java/com/codecool/dungeoncrawl/Tiles.java
@@ -1,11 +1,5 @@
 package com.codecool.dungeoncrawl;
 
-import com.codecool.dungeoncrawl.logic.CellType;
-import com.codecool.dungeoncrawl.logic.Drawable;
-import com.codecool.dungeoncrawl.logic.actor.monsters.MonsterType;
-import com.codecool.dungeoncrawl.logic.items.ConsumableType;
-import com.codecool.dungeoncrawl.logic.items.KeyType;
-import com.codecool.dungeoncrawl.logic.items.WeaponType;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
@@ -13,9 +7,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Tiles {
-    public static int TILE_WIDTH = 32;
-    private static final int numberOfrTiles = 1024;
-    private static final int rowLength = 32;
+    public static final int TILE_WIDTH = 32;
+    private static final int NUMBER_OF_TILES = 1024;
 
     private static final Image tileset = new Image("/tiles.png", 543 * 2, 543 * 2, true, false);
     private static final Map<Integer, Tile> tileMap = new HashMap<>();
@@ -30,11 +23,11 @@ public class Tiles {
     }
 
     static {
-        for (int tileID = 0; tileID < numberOfrTiles; tileID++) {
-            int row = tileID / (rowLength);
-            int col = tileID % (rowLength);
-//            System.out.printf("%s: (%s,%s)%n", tileID, row, col);
-            tileMap.put(tileID, new Tile(col,row));
+        for (int tileId = 0; tileId < NUMBER_OF_TILES; tileId++) {
+            int row = tileId / (TILE_WIDTH);
+            int col = tileId % (TILE_WIDTH);
+//            System.out.printf("%s: (%s,%s)%n", tileId, row, col);
+            tileMap.put(tileId, new Tile(col,row));
         }
     }
 
