a8e6b2e29da40150f2ecd11ea77f6013b683fac2
dudaskobende <dudaskobende@gmail.com>
2022-05-02 19:48:17 +0200

    Extends PlayerDaoJdbc.add method

--- src/main/java/com/codecool/dungeoncrawl/dao/PlayerDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/PlayerDaoJdbc.java
@@ -16,12 +16,33 @@ public class PlayerDaoJdbc implements PlayerDao {
     @Override
     public void add(PlayerModel player) {
         try (Connection conn = dataSource.getConnection()) {
-            String sql = "INSERT INTO player (player_name, hp, x, y) VALUES (?, ?, ?, ?)";
+            String sql = "INSERT INTO " +
+                    "player (" +
+                    "player_name," +
+                    "hp, " +
+                    "max_hp, " +
+                    "fireball_timer, " +
+                    "damage, " +
+                    "direction_type, " +
+                    "cell_type, " +
+                    "weapon_type, " +
+                    "items, " +
+                    "x, " +
+                    "y ) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             statement.setString(1, player.getPlayerName());
             statement.setInt(2, player.getHp());
-            statement.setInt(3, player.getX());
-            statement.setInt(4, player.getY());
+            statement.setInt(3, player.getMaxHp());
+            statement.setInt(4, player.getFireballTimer());
+            statement.setInt(5, player.getDamage());
+            statement.setString(6, player.getDirectionType());
+            statement.setInt(7, player.getCellType());
+            statement.setInt(8, player.getWeaponType());
+            Array items = conn.createArrayOf("INTEGER", player.getItems().toArray(new Integer[0]));
+            statement.setArray(9, items);
+            statement.setInt(10, player.getX());
+            statement.setInt(11, player.getY());
             statement.executeUpdate();
             ResultSet resultSet = statement.getGeneratedKeys();
             resultSet.next();
