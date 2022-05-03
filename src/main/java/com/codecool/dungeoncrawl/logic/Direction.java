789ea4d3c917883dc1c1c109cc99d902ddea4589
dudaskobende <dudaskobende@gmail.com>
2022-05-03 07:08:43 +0200

    Add id for Direction enum

--- src/main/java/com/codecool/dungeoncrawl/logic/Direction.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/Direction.java
@@ -1,17 +1,19 @@
 package com.codecool.dungeoncrawl.logic;
 
 public enum Direction {
-    UP(0, -1),
-    DOWN(0, 1),
-    LEFT(-1, 0),
-    RIGHT(1, 0);
+    UP(0, -1, 1),
+    DOWN(0, 1, 2),
+    LEFT(-1, 0, 3),
+    RIGHT(1, 0, 4);
 
     private final int x;
     private final int y;
+    private final int ID;
 
-    Direction(int x, int y) {
+    Direction(int x, int y, int id) {
         this.x = x;
         this.y = y;
+        ID = id;
     }
 
     public int getX() {
@@ -20,5 +22,9 @@ public enum Direction {
 
     public int getY() {
         return y;
+    }
+
+    public int getID() {
+        return ID;
     }
 }
