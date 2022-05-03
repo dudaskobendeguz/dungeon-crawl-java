162b0f1276c34530c2969c723d8677cb7bc6d7df
benedekhalaj <bendihalaj@gmail.com>
2022-05-03 17:09:26 +0200

    Add MonsterModel fields, constructors, getters and setters

--- src/main/java/com/codecool/dungeoncrawl/model/MonsterModel.java
+++ src/main/java/com/codecool/dungeoncrawl/model/MonsterModel.java
@@ -1,4 +1,64 @@
 package com.codecool.dungeoncrawl.model;
 
 public class MonsterModel extends BaseModel {
+    private int typeId;
+    private int x;
+    private int y;
+    private int hp;
+    private int directionId;
+
+    public MonsterModel(int typeId, int x, int y, int hp) {
+        this.typeId = typeId;
+        this.x = x;
+        this.y = y;
+        this.hp = hp;
+    }
+
+    public MonsterModel(int typeId, int x, int y, int hp, int directionId) {
+        this.typeId = typeId;
+        this.x = x;
+        this.y = y;
+        this.hp = hp;
+        this.directionId = directionId;
+    }
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(int typeId) {
+        this.typeId = typeId;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getDirectionId() {
+        return directionId;
+    }
+
+    public void setDirectionId(int directionId) {
+        this.directionId = directionId;
+    }
 }
