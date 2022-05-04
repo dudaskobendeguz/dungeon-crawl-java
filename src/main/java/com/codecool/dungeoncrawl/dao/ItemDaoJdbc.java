f1d5398892cd07fc8fb8a37e08867b12a131bd43
benedekhalaj <bendihalaj@gmail.com>
2022-05-04 10:18:43 +0200

    Add ItemDaoJdbc.add method

--- src/main/java/com/codecool/dungeoncrawl/dao/ItemDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/ItemDaoJdbc.java
@@ -3,6 +3,7 @@ package com.codecool.dungeoncrawl.dao;
 import com.codecool.dungeoncrawl.model.ItemModel;
 
 import javax.sql.DataSource;
+import java.sql.*;
 import java.util.List;
 
 public class ItemDaoJdbc implements ItemDao {
@@ -15,7 +16,22 @@ public class ItemDaoJdbc implements ItemDao {
 
     @Override
     public void add(ItemModel itemModel, int gameStateId) {
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "INSERT INTO item (game_state_id, type_id, x, y) VALUES (?, ?, ?, ?)";
+            PreparedStatement st = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            st.setInt(1, gameStateId);
+            st.setInt(2, itemModel.getTileId());
+            st.setInt(3, itemModel.getX());
+            st.setInt(4, itemModel.getY());
 
+            st.executeUpdate();
+            ResultSet rs = st.getGeneratedKeys();
+            rs.next();
+            int id = rs.getInt(1);
+            itemModel.setId(id);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
