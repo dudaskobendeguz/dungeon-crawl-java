5b4f0489b312dedcf8174a8adca96af75149ec40
dudaskobende <dudaskobende@gmail.com>
2022-05-04 09:22:52 +0200

    Add GameDatabaseManager.createNewPlayer

--- src/main/java/com/codecool/dungeoncrawl/dao/GameDatabaseManager.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameDatabaseManager.java
@@ -2,13 +2,14 @@ package com.codecool.dungeoncrawl.dao;
 
 import com.codecool.dungeoncrawl.Level;
 import com.codecool.dungeoncrawl.Tiles;
-import com.codecool.dungeoncrawl.logic.GameMap;
+import com.codecool.dungeoncrawl.logic.*;
 import com.codecool.dungeoncrawl.logic.actor.monsters.Monster;
 import com.codecool.dungeoncrawl.logic.actor.monsters.MoveDirection;
 import com.codecool.dungeoncrawl.logic.actor.monsters.MoveTimer;
-import com.codecool.dungeoncrawl.logic.MapLoader;
 import com.codecool.dungeoncrawl.logic.actor.player.Player;
 import com.codecool.dungeoncrawl.logic.items.Item;
+import com.codecool.dungeoncrawl.logic.items.Weapon;
+import com.codecool.dungeoncrawl.logic.items.WeaponType;
 import com.codecool.dungeoncrawl.model.GameState;
 import com.codecool.dungeoncrawl.model.MonsterModel;
 import com.codecool.dungeoncrawl.model.PlayerModel;
@@ -87,14 +88,52 @@ public class GameDatabaseManager {
     public GameMap loadGame(int gameStateId) {
         Level loadedLevel = loadGameState(gameStateId);
         PlayerModel playerModel = loadPlayer(gameStateId);
-        List<Item> inventoryItems = createItemsFromDbData(playerModel.getItems());
-        createNewPlayer(playerModel);
+        Player player = createNewPlayer(playerModel);
         List<CellModel> cellModels = loadCells(gameStateId);
 //        GameMap map = MapLoader.getGameMap(loadedLevel.getMAP_FILE_PATH(),)
         return null;
     }
 
-    private List<Item> createItemsFromDbData(List<Integer> itemsTileIds) {
+    private Player createNewPlayer(PlayerModel playerModel) {
+        List<Item> items = createItemsFromData(playerModel.getItems());
+        Cell cell = createCellFromData(playerModel.getX(), playerModel.getY(), playerModel.getCellTypeId());
+        Weapon weapon = createWeaponFromData(playerModel.getWeaponTypeId());
+        Direction direction = createDirectionFromData(playerModel.getDirectionTypeId());
+        return new Player(playerModel, cell, direction, weapon, items);
+    }
+
+    private Direction createDirectionFromData(int directionTypeId) {
+        return Arrays.stream(Direction.values())
+                .filter(direction -> direction.getID() == directionTypeId)
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+    }
+
+    private Weapon createWeaponFromData(int weaponTypeId) {
+        WeaponType weaponType = getWeaponType(weaponTypeId);
+        return new Weapon(null, weaponType);
+    }
+
+    private WeaponType getWeaponType(int weaponTypeId) {
+        return Arrays.stream(WeaponType.values())
+                .filter(weaponType -> weaponType.getTileId() == weaponTypeId)
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+    }
+
+    private Cell createCellFromData(int x, int y, int cellTypeId) {
+        CellType cellType = getCellType(cellTypeId);
+        return new Cell(x, y, cellType);
+    }
+
+    private CellType getCellType(int cellTypeId) {
+        return Arrays.stream(CellType.values())
+                .filter(cellType -> cellType.getTileId() == cellTypeId)
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+    }
+
+    private List<Item> createItemsFromData(List<Integer> itemsTileIds) {
         List<Item> items = new ArrayList<>();
         for (Integer itemsTileId : itemsTileIds) {
             Item item = MapLoader.createItem(null, Tiles.tileTypeMap.get(itemsTileId));
@@ -118,12 +157,6 @@ public class GameDatabaseManager {
 
     public PlayerModel loadPlayer(int gameStateId) {
         return playerDao.get(gameStateId);
-    }
-
-    private Player createNewPlayer(PlayerModel playerModel) {
-        //TODO implement the loading player phase
-        System.out.println(playerModel);
-        return null;
     }
 
     public List<CellModel> loadMap() {
